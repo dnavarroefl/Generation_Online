1461d4468097709655518741980f0d3bf334ad86
dnavarroefl <dnavarroefl@hotmail.com>
2015-04-14 17:41:51 +0200

    Corrección del Object Oriented Blog
    Añadidas las correcciones del 1er ejercicio (week1).

--- week1_exercises/Object oriented blog.rb
+++ week1_exercises/Object oriented blog.rb
@@ -1,47 +1,64 @@
-class Blog
-    
-    def publish_front_page
-    end 
-    def create_front_page
-    end 
-    
-    blog = Blog.new
-    blog.publish_front_page
-    blog.create_front_page
-end
-
 class Post
     
-    attr_accessor :title, :date, :text, :number
-	def initialize (title, date, text, number)
+    attr_accessor :title, :date, :text
+	def initialize title, date, text
 	@title = title
 	@date = date
 	@text = text
-	@number = number
 	end 
 end 
 
-    def title
-        puts "Post title", "**************"
+class Blog
+    attr_accesor :posts
+    def initialize
+    	@posts = []
     end 
-    def date
-        puts Time.now
-    end
-    def text
-        puts "Post text", "----------------"
-    end
+    
+    def add_post post
+    	@posts << post
+    end 
+    
+    def create_front_page
+    	front_page = ""
+    	
+    	sort_posts
+    
+    	@posts.each do|posts|
+    	front_page << post.title
+    	front_page << decorate_post_head
+    	front_page << post.text
+    	front_page << decorate_post_body
+    	
+    	front_page
+     end
+     
+     def sort_posts
+     	@posts.sort!{|a,b|b.date <=>a.date}
+     end 
+    	
+    def show_front_page
+    	system("clear")
+    	puts create_front_page
+    end 
+    
+    def decorate_post_head
+    	"\n******************\n"
+    end 
+    
+    def decorate_post_body
+    	"\n==================\n\n"
+	end
+end
 
-3.times do 
-    puts title, text, date 
-end 
-3.times do 
-    puts title, text, date 
-end 
-2.times do 
-    puts title, text, date
-end 
-<<<<<<< HEAD
-=======
+blog = Blog.new
 
+post1 = Post.new( "Nuevo post", Time.now, "Va a ser el mejor blog del mundo" )
+post2 = Post.new( "¡¡Bienvenidos!!", (Time.now) -1000, "Presentando el blog!" )
+post3 = Post.new( "Despedida", Time.now, "Ni una sola visita :(" )
 
->>>>>>> origin/master
+blog.add_post post1
+blog.add_post post2
+blog.add_post post3
+
+blog.show_front_page
+
